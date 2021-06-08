420eecf98ea6b670a79598c2abc376f3e81e7f1f
matyushkin <leva.matyushkin@gmail.com>
2021-06-08 12:09:06 +0300

    Django Cookiecutter is added

---
+++ title/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from title.users.models import User
+from title.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
